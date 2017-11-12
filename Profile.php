b38d93c7fb7b95c1a7e370a9f870fcb23ca3abee
TheAetherGod <33580943+TheAetherGod@users.noreply.github.com>
2017-11-11 21:57:31 -0600

    Updated Profile with header

--- Profile.php
+++ Profile.php
@@ -1,16 +1,15 @@
 <html>
- <head
-   <title>Profile</title>
- </head>
+<head>
+   		<meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link type="text/css" rel="stylesheet" href="css/index.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		<title></title>
+	</head>
  <body>
-     <div id="header">
-         <img src="logo.png" alt="Whoa" />
-     </div>
-     <div id="content">
-         <p>Awesome content goes here</p>
-     </div>
+ <?php include 'nav.php';?>
+ </body>
      <div id="footer">
          <p>Copyright 2017 EzRoomy.</p>
-     </div>
- </body>
  </html>
