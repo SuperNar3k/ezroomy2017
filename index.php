7e3c91555aad1f0a84810ea7cd5a0ea95f1ab9f5
TheAetherGod <33580943+TheAetherGod@users.noreply.github.com>
2017-11-11 21:50:53 -0600

    1

---
+++ index.php
@@ -0,0 +1,26 @@
+@@ -1,25 +0,0 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+   		<meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link type="text/css" rel="stylesheet" href="css/index.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		<title></title>
+	</head>
+	<body>
+		<?php include "nav.php";?>
+		<div class="center"></div>
+		<div id="footer">
+		<a id="bottomtext">About us</a>
+		<a id="bottomtext">News</a>
+		<a id="bottomtext">Feedback</a>
+		<a id="bottomtext">FAQ</a>
+		<a id="bottomtext">Privacy Policy</a>
+		<a id="bottomtext">Terms of Use</a>
+		<a id="bottomtext">Help</a>
+		<a id="bottomtext">Contact Us</a>
+		</div>
+	</body>
+</html>
