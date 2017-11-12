82428ab796ebb4f7cf83e2699f39b71ad5f57c39
Benwagrez <benwagrez@gmail.com>
2017-11-12 02:55:39 -0600

    x dfb

--- userprofile.php
+++ userprofile.php
@@ -47,12 +47,3 @@ session_start();
         }
     }
      ?>
-        
-
-		<!-- <div class="header">
-            <img class="icon" src="icon/hiresweb.png" alt="Icon">
-			<a class="name">EZROOMY</a>
-			<button class="homeprflbutton">Home Profile<i class="fa fa-caret-down"></i></button>
-            <button class="userprflbutton">User Profile<i class="fa fa-caret-down"></i></button>
-            <a class="menus" href="login.php"> Sign-up/Log-in </a>
-		</div> -->
