eae8dcbc941c75f81b906cb3f4de78f9dbb96444
Narek Ohanyan <ohanyanarek@gmail.com>
2017-11-11 22:42:34 -0600

    css

--- Profile.php
+++ Profile.php
@@ -1,4 +1,5 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
    		<meta charset="UTF-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,9 +8,17 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
 		<title></title>
 	</head>
- <body>
+<body>
  <?php include 'nav.php';?>
- </body>
-     <div id="footer">
-         <p>Copyright 2017 EzRoomy.</p>
- </html>
+ <div class="center"></div>
+ <div id="footer">
+			<a id="bottomtext">About us</a>
+			<a id="bottomtext">News</a>
+			<a id="bottomtext">Feedback</a>
+			<a id="bottomtext">FAQ</a>
+			<a id="bottomtext">Privacy Policy</a>
+			<a id="bottomtext">Terms of Use</a>
+			<a id="bottomtext">Help</a>
+			<a id="bottomtext">Contact Us</a>
+</body>
+</html>
