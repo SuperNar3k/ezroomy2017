905adc4b0189dde16832c35160c22cefc23fa562
Benwagrez <benwagrez@gmail.com>
2017-11-12 05:22:37 -0600

    asgsgsdf

--- homeprofile.php
+++ homeprofile.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/homeprofile.css">
 
@@ -84,6 +83,6 @@
             </tr>
     </table>
     </div>
-    <?php include "footer.php"; ?>  
+    <?php include "footer.php"; ?> 
 </body>
 </html>
