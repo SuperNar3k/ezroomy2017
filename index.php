d9ad597ba68648b32fa8e186b6dddd9fbbb698e2
Narek Ohanyan <ohanyanarek@gmail.com>
2017-11-11 22:31:36 -0600

    css

--- index.php
+++ index.php
@@ -12,14 +12,15 @@
 		<?php include "nav.php";?>
 		<div class="center"></div>
 		<div id="footer">
-		<a id="bottomtext">About us</a>
-		<a id="bottomtext">News</a>
-		<a id="bottomtext">Feedback</a>
-		<a id="bottomtext">FAQ</a>
-		<a id="bottomtext">Privacy Policy</a>
-		<a id="bottomtext">Terms of Use</a>
-		<a id="bottomtext">Help</a>
-		<a id="bottomtext">Contact Us</a>
+			<a id="bottomtext">About us</a>
+			<a id="bottomtext">News</a>
+			<a id="bottomtext">Feedback</a>
+			<a id="bottomtext">FAQ</a>
+			<a id="bottomtext">Privacy Policy</a>
+			<a id="bottomtext">Terms of Use</a>
+			<a id="bottomtext">Help</a>
+			<a id="bottomtext">Contact Us</a>
+			<a>©EzRoomy</a>
 		</div>
 	</body>
 </html>
