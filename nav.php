5bd3f265d654b318b936ec4cbde603ffbaecd02e
Benwagrez <benwagrez@gmail.com>
2017-12-07 21:14:52 -0600

    Nav Bar integration for Home Builder

--- nav.php
+++ nav.php
@@ -2,6 +2,7 @@
     <a class="name" href="index.php"><img class="icon" src="icon/hiresweb.png" alt="Icon">EZROOMY</a>
     <?php if(isset($_SESSION["Username"])) : ?>
         <a class="menus" href="homeprofile.php">Home Profile</a>
+        <a class="menus" href="homebuilder.php">Home Editor</a>
         <a class="menus" href="userprofile.php">User Profile</a>
         <a class="menus" href="logout.php">Log-out</a>
     <?php else : ?>
