db7ff74bd2ef8f8a1c32cee4015d120352a15fac
Benwagrez <benwagrez@gmail.com>
2017-12-10 16:28:36 -0600

    Updating Database file

--- usertables.sql
+++ usertables.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2017 at 09:13 AM
+-- Generation Time: Dec 10, 2017 at 11:27 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -32,53 +32,18 @@ CREATE TABLE `bill` (
   `id` int(12) NOT NULL,
   `name` varchar(20) NOT NULL,
   `value` int(11) NOT NULL,
-  `due date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `home`
---
-
-CREATE TABLE `home` (
-  `id` int(12) NOT NULL,
-  `address` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `homebill`
---
-
-CREATE TABLE `homebill` (
-  `homeid` int(12) NOT NULL,
-  `billid` int(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(12) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `phonenumber` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `homeid` int(12) NOT NULL
+  `due date` date NOT NULL,
+  `rp` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `bill`
 --
 
-INSERT INTO `user` (`id`, `username`, `email`, `phonenumber`, `password`, `homeid`) VALUES
-(2, 'ben', 'ben@me.com', '123123123', '1234', 1234),
-(4, 'igloo', 'khan@me.com', '555666777', '12345', 0);
+INSERT INTO `bill` (`id`, `name`, `value`, `due date`, `rp`) VALUES
+(51, 'Electricity', 60129, '2007-12-17', 'Hank Adams'),
+(52, 'your mom', 69, '0000-00-00', 'Bubbles'),
+(53, '12', 2, '0000-00-00', '1');
 
 --
 -- Indexes for dumped tables
@@ -91,18 +56,6 @@ ALTER TABLE `bill`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `home`
---
-ALTER TABLE `home`
-  ADD PRIMARY KEY (`id`) USING BTREE;
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -110,19 +63,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `bill`
 --
 ALTER TABLE `bill`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `home`
---
-ALTER TABLE `home`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
